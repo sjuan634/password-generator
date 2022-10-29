cc37c7de0398865c98449bba3a12f4721993e588
sanchezju <sjuan634@yahoo.com>
2022-10-28 21:24:42 -0700

    add variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,9 @@
 // Assignment code here
-
+let lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+let upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let numbers = [0,1,2,3,4,5,6,7,8,9]
+let specialCharacters = "~!@#$%^&*()_-+={[}]|:;'<,>.?/";
+let numCharacters = prompt("How many characters would you like the password to be? (8 - 128)");
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
